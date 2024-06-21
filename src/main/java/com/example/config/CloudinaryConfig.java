93cb594b3bba01f6b5630908657167589ae286a5
tranngochuy572003 <tranngochuy1234a@gmail.com>
2024-06-21 13:38:03 +0700

    [ogani-122] Apply .env file to project

--- src/main/java/com/example/config/CloudinaryConfig.java
+++ src/main/java/com/example/config/CloudinaryConfig.java
@@ -1,7 +1,7 @@
 package com.example.config;
 
 import com.cloudinary.Cloudinary;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,13 +10,22 @@ import java.util.Map;
 
 @Configuration
 public class CloudinaryConfig {
+
+    @Value("${server.cloud_name}")
+    private String cloudName;
+
+    @Value("${server.api_key}")
+    private String apiKey;
+
+    @Value("${server.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary(){
-        Dotenv dotenv = Dotenv.load();
         Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"));
-        config.put("api_key",dotenv.get("CLOUDINARY_API_KEY"));
-        config.put("api_secret",dotenv.get("CLOUDINARY_API_SECRET"));
+        config.put("cloud_name", cloudName);
+        config.put("api_key",apiKey);
+        config.put("api_secret",apiSecret);
 
         return new Cloudinary(config);
     }
