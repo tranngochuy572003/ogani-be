a5bdd672b676229d30122c35b15f3e129834de64
tranngochuy572003 <tranngochuy1234a@gmail.com>
2024-07-09 11:39:52 +0700

    [ogani-172] User's cart doesn't delete when there isn't product in the cart

--- src/main/java/com/example/repository/CartRepository.java
+++ src/main/java/com/example/repository/CartRepository.java
@@ -3,12 +3,18 @@ package com.example.repository;
 import com.example.entity.Cart;
 import com.example.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface CartRepository  extends JpaRepository<Cart,String> {
     Cart findByUsers(User user);
     @Query("SELECT ca FROM carts ca WHERE ca.users.id = ?1 ")
     Cart findByUserId(String userId);
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM carts  ca WHERE ca.id = :cartId")
+    void deleteCartById(String cartId);
 }
